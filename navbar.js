cbd78bab3e89ee593444a8e2ee7e22ebb838c63d
zanzan13531 <zanzan13531@gmail.com>
2022-07-07 13:40:36 -0700

    added in a bunch of blank pages with the future topics

--- navbar.js
+++ navbar.js
@@ -11,8 +11,8 @@ document.write('<nav class="navbar navbar-inverse">\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
         <li><a href="subpages/k_droppedFromRest.html">Object Dropped From Rest</a></li>\
-        <li><a href="subpages/hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="subpages/topic_c.html">Topic C</a></li>\
+        <li><a href="subpages/k_thrownVertically.html">Object Thrown Vertically</a></li>\
+        <li><a href="subpages/k_1DMotion.html">One Dimentional Motion</a></li>\
       </ul>\
     </li>\
     \
@@ -20,9 +20,12 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Forces/Dynamics\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="subpages/addTwoNumbers.html">Add Two Numbers</a></li>\
-        <li><a href="subpages/hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="subpages/topic_c.html">Topic C</a></li>\
+        <li><a href="subpages/f_angledForces.html">Angled Forces</a></li>\
+        <li><a href="subpages/f_atwoodMachine.html">Atwood Machine</a></li>\
+        <li><a href="subpages/f_blockOnSlope.html">A Block on a Slope</a></li>\
+        <li><a href="subpages/f_forceToAcceleration.html">Force to Acceleration</a></li>\
+        <li><a href="subpages/f_kineticFriction.html">Kinetic Friction</a></li>\
+        <li><a href="subpages/f_staticFriction.html">Static Friction</a></li>\
       </ul>\
     </li>\
     \
@@ -30,9 +33,7 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Energy/Work/Power\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="subpages/addTwoNumbers.html">Add Two Numbers</a></li>\
-        <li><a href="subpages/hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="subpages/topic_c.html">Topic C</a></li>\
+        <li><a href="subpages/e_conservationOfEnergy.html">Conservation of Energy</a></li>\
       </ul>\
     </li>\
     \
@@ -40,9 +41,7 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Momentum\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="subpages/addTwoNumbers.html">Add Two Numbers</a></li>\
-        <li><a href="subpages/hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="subpages/topic_c.html">Topic C</a></li>\
+        <li><a href="subpages/m_collision.html">Collisions</a></li>\
       </ul>\
     </li>\
     \
@@ -50,9 +49,9 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Rotation\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="subpages/addTwoNumbers.html">Add Two Numbers</a></li>\
-        <li><a href="subpages/hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="subpages/topic_c.html">Topic C</a></li>\
+        <li><a href="subpages/r_ballOnSlope.html">A Ball on a Slope</a></li>\
+        <li><a href="subpages/r_basicTorque.html">Torque</a></li>\
+        <li><a href="subpages/r_centripitalMotion.html">Centripital Motion</a></li>\
       </ul>\
     </li>\
     \
@@ -60,9 +59,8 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Oscillation\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="subpages/addTwoNumbers.html">Add Two Numbers</a></li>\
-        <li><a href="subpages/hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="subpages/topic_c.html">Topic C</a></li>\
+        <li><a href="subpages/o_pendulum.html">Pendulums</a></li>\
+        <li><a href="subpages/o_spring.html">Springs</a></li>\
       </ul>\
     </li>\
     \
@@ -70,9 +68,8 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Gravity\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="subpages/addTwoNumbers.html">Add Two Numbers</a></li>\
-        <li><a href="subpages/hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="subpages/topic_c.html">Topic C</a></li>\
+        <li><a href="subpages/g_centripitalOrbit.html">Centripital Orbit</a></li>\
+        <li><a href="subpages/g_gravityOnAnotherPlanet.html">Gravity on Another Planet</a></li>\
       </ul>\
     </li>\
     \
--- subpages/subNavbar.js
+++ subpages/subNavbar.js
@@ -11,8 +11,8 @@ document.write('<nav class="navbar navbar-inverse">\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
         <li><a href="k_droppedFromRest.html">Object Dropped From Rest</a></li>\
-        <li><a href="hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="topic_c.html">Topic C</a></li>\
+        <li><a href="k_thrownVertically.html">Object Thrown Vertically</a></li>\
+        <li><a href="k_1DMotion.html">One Dimentional Motion</a></li>\
       </ul>\
     </li>\
     \
@@ -20,9 +20,12 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Forces/Dynamics\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="addTwoNumbers.html">Add Two Numbers</a></li>\
-        <li><a href="hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="topic_c.html">Topic C</a></li>\
+        <li><a href="f_angledForces.html">Angled Forces</a></li>\
+        <li><a href="f_atwoodMachine.html">Atwood Machine</a></li>\
+        <li><a href="f_blockOnSlope.html">A Block on a Slope</a></li>\
+        <li><a href="f_forceToAcceleration.html">Force to Acceleration</a></li>\
+        <li><a href="f_kineticFriction.html">Kinetic Friction</a></li>\
+        <li><a href="f_staticFriction.html">Static Friction</a></li>\
       </ul>\
     </li>\
     \
@@ -30,9 +33,7 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Energy/Work/Power\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="addTwoNumbers.html">Add Two Numbers</a></li>\
-        <li><a href="hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="topic_c.html">Topic C</a></li>\
+        <li><a href="e_conservationOfEnergy.html">Conservation of Energy</a></li>\
       </ul>\
     </li>\
     \
@@ -40,9 +41,7 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Momentum\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="addTwoNumbers.html">Add Two Numbers</a></li>\
-        <li><a href="hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="topic_c.html">Topic C</a></li>\
+        <li><a href="m_collision.html">Collisions</a></li>\
       </ul>\
     </li>\
     \
@@ -50,9 +49,9 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Rotation\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="addTwoNumbers.html">Add Two Numbers</a></li>\
-        <li><a href="hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="topic_c.html">Topic C</a></li>\
+        <li><a href="r_ballOnSlope.html">A Ball on a Slope</a></li>\
+        <li><a href="r_basicTorque.html">Torque</a></li>\
+        <li><a href="r_centripitalMotion.html">Centripital Motion</a></li>\
       </ul>\
     </li>\
     \
@@ -60,9 +59,8 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Oscillation\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="addTwoNumbers.html">Add Two Numbers</a></li>\
-        <li><a href="hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="topic_c.html">Topic C</a></li>\
+        <li><a href="o_pendulum.html">Pendulums</a></li>\
+        <li><a href="o_spring.html">Springs</a></li>\
       </ul>\
     </li>\
     \
@@ -70,9 +68,8 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Gravity\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="addTwoNumbers.html">Add Two Numbers</a></li>\
-        <li><a href="hPlusToPh.html">[H+] to pH</a></li>\
-        <li><a href="topic_c.html">Topic C</a></li>\
+        <li><a href="g_centripitalOrbit.html">Centripital Orbit</a></li>\
+        <li><a href="g_gravityOnAnotherPlanet.html">Gravity on Another Planet</a></li>\
       </ul>\
     </li>\
     \
